5681c4953696c1d35fe517108736c01a13909aac
danielm <daniel.mosko@upjs.sk>
2024-08-09 19:58:54 +0200

    New Zed editor config, nvim which-key fix

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -1,53 +1,15 @@
----@param command string
----@return fun(command: string)
-local function cmd(command)
-    return function()
-        vim.cmd(command)
-    end
-end
-
 return {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-        'famiu/bufdelete.nvim',
-        'nvim-tree/nvim-web-devicons',
-        { 'echasnovski/mini.icons', version = false },
-    },
+  "folke/which-key.nvim",
     config = function()
-        local wk = require 'which-key'
+      -- Setup which-key
+      local wk = require("which-key")
+      wk.setup {
+        -- Your configuration options here
+      }
 
-        local mappings = {
-            {
-                mode = { 'n', 'v' },
-                { '<leader>c', group = 'Close' },
-
-                { '<leader>d', cmd 'Bdelete', desc = 'Delete Current Buffer' },
-
-                { '<leader>w', group = 'Window Layout' },
-                { '<leader>wh', cmd 'abo vsplit', desc = 'Split left' },
-                { '<leader>wj', cmd 'split', desc = 'Split bottom' },
-                { '<leader>wk', cmd 'to split', desc = 'Split top' },
-                { '<leader>wl', cmd 'vsplit', desc = 'Split right' },
-                { '<leader>wv', cmd 'vsplit', desc = 'Split right' },
-
-                { '<leader>g', group = 'Git' },
-                { '<leader>gh', group = 'Git Hunk' },
-                { '<leader>l', group = 'LSP' },
-            },
-        }
-
-        wk.setup {
-            win = {
-                border = 'rounded',
-                padding = { 1, 0, 1, 0 },
-            },
-        }
-
-        wk.add(mappings)
-    end,
+    end
 }
--- retrn {
+-- return {
 --   "folke/which-key.nvim",
 --   event = "VeryLazy",
 --   opts = {
