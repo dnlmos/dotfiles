c4726cef77ffe5643d63a6aabbedfef922e35ec3
danielm <daniel.mosko@upjs.sk>
2024-11-02 14:10:26 +0100

    zshrc update etc.

--- .wezterm.lua
+++ .wezterm.lua
@@ -49,7 +49,7 @@ config.font_size = 15
 config.enable_tab_bar = true
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.95
+-- config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
