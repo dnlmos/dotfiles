9019f3fc23e4d1e051ef494978ca95f6dd74c15d
daniel-mosko <danielmosko5@gmail.com>
2025-06-13 19:34:33 +0200

    Fix: Load options first to set leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
+require("core.options")
 require("core.autocmds")
 require("core.keymaps")
-require("core.options")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
