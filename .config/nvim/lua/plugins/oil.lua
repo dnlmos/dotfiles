2397fea6aec778cb6cf6bbf7465c06c79f648348
daniel-mosko <danielmosko5@gmail.com>
2025-07-18 12:53:06 +0200

    Cleanup, replace neo-tree with oil, minor change to colorscheme

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,50 @@
+return {
+  "stevearc/oil.nvim",
+  event = "VeryLazy",
+  opts = {},
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Oil-Open parent directory" }),
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end,
+      },
+      float = {
+        padding = 2,
+        max_width = 80,
+        max_height = 40,
+      },
+      win_options = {
+        wrap = true,
+        winblend = 0,
+      },
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = false,
+        ["q"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["<BS>"] = { "actions.parent", mode = "n" },
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+    })
+  end,
+}
