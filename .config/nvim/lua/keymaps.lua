3e2e2fb207499c3defcfafa4aea5c79dc93f0b2e
daniel-mosko <danielmosko5@gmail.com>
2025-11-02 08:30:47 +0100

    feat: enhance keymaps and add auto-session
    - Add `which-key` groups for different functionalities.
    - Add keymaps for window and buffer management.
    - Replace persistence.nvim with auto-session.
    - Disable gitsigns signcolumn.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,8 +1,25 @@
 vim.pack.add({
   { src = "https://github.com/folke/which-key.nvim" },
 })
-require("which-key").setup({
+local wk = require("which-key")
+
+wk.setup({
   preset = "helix",
+})
+
+-- Register groups appear
+wk.add({
+  { "<leader>w", group = "+Window" },
+  { "<leader>b", group = "+Buffer" },
+  { "<leader>p", group = "+Plugin/Package" },
+  { "<leader>t", group = "+Tab" },
+  { "<leader>a", group = "+AutoSession" },
+  { "<leader>c", group = "+Code/LSP" },
+  { "<leader>f", group = "+Find/File" },
+  { "<leader>s", group = "+Search/Grep" },
+  { "<leader>g", group = "+Git" },
+  { "<leader>o", group = "+Open/Command" },
+  { "<leader>d", group = "+Diagnostics" },
 })
 
 local keymap = vim.keymap.set
@@ -10,6 +27,8 @@ local s = { silent = true }
 
 keymap("n", "<space>", "<Nop>")
 
+-- Clear search highlighting on Escape
+vim.keymap.set('n', '<Esc>', '<Cmd>nohlsearch<CR>', { desc = 'Clear search highlighting' })
 -- ── Plugin manager ───────────────────────────────────
 keymap("n", "<leader>ps", "<cmd>lua vim.pack.update()<CR>")
 -- movement
@@ -18,9 +37,9 @@ keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 keymap("n", "<C-d>", "<C-d>zz")
 keymap("n", "<C-u>", "<C-u>zz")
 
---- save and quit
-keymap("n", "<leader>w", "<cmd>w!<CR>", s)
-keymap("n", "<leader>q", "<cmd>q<CR>", s)
+-- --- save and quit
+-- keymap("n", "<leader>w", "<cmd>w!<CR>", s)
+-- keymap("n", "<leader>q", "<cmd>q<CR>", s)
 
 -- tabs
 keymap("n", "<leader>te", "<cmd>tabnew<CR>", s)
@@ -44,21 +63,62 @@ vim.keymap.set('n', '<leader>go', function()
   require("mini.diff").toggle_overlay(0)
 end, { desc = "Toggle mini.diff overlay" })
 
--- Persistence (Session Manager)
-keymap("n", "<leader>Qs", function() require("persistence").load() end, { desc = "Restore Session" })
-keymap("n", "<leader>QS", function() require("persistence").select() end, { desc = "Select Session to Load" })
-keymap("n", "<leader>Ql", function() require("persistence").load({ last = true }) end, { desc = "Restore Last Session" })
-keymap("n", "<leader>Qd", function() require("persistence").stop() end, { desc = "Don't Save Current Session" })
+-- Autosession
+vim.keymap.set('n', '<leader>ar', "<Cmd>AutoSession search<CR>", { desc = 'Session search' })
+vim.keymap.set('n', '<leader>as', "<Cmd>AutoSession save<CR>", { desc = 'Save session' })
+vim.keymap.set('n', '<leader>aa', "<Cmd>AutoSession toggle<CR>", { desc = 'Toggle autosave' })
 
 -- cd current dir
 keymap("n", "<leader>cd", '<cmd>lua vim.fn.chdir(vim.fn.expand("%:p:h"))<CR>')
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- === WINDOW NAVIGATION (Ctrl + hjkl) ===
+keymap("n", "<C-h>", "<C-w>h", { desc = "Move focus to the left window" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Move focus to the right window" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Move focus to the lower window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Move focus to the upper window" })
+
+-- === <leader>w : Window Management ===
+keymap("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontally" })
+keymap("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+keymap("n", "<leader>wc", "<C-w>c", { desc = "Close current window" })
+keymap("n", "<leader>wo", "<C-w>o", { desc = "Close all other windows" })
+keymap("n", "<leader>w=", "<C-w>=", { desc = "Equalize window sizes" })
+
+-- Resize with arrows (hold Ctrl + Arrow)
+keymap("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+keymap("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+-- Move windows (swap positions)
+keymap("n", "<leader>wh", "<C-w>H", { desc = "Move window to far left" })
+keymap("n", "<leader>wl", "<C-w>L", { desc = "Move window to far right" })
+keymap("n", "<leader>wj", "<C-w>J", { desc = "Move window to bottom" })
+keymap("n", "<leader>wk", "<C-w>K", { desc = "Move window to top" })
+
+-- Rotate / exchange
+keymap("n", "<leader>wr", "<C-w>r", { desc = "Rotate windows clockwise" })
+keymap("n", "<leader>wR", "<C-w>R", { desc = "Rotate windows counter-clockwise" })
+keymap("n", "<leader>wx", "<C-w>x", { desc = "Exchange with next window" })
+
+-- === <leader>b : Buffer Management ===
+keymap("n", "<leader>bb", "<cmd>b#<CR>", { desc = "Switch to last buffer" })
+keymap("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete current buffer" })
+keymap("n", "<leader>bD", "<cmd>bdelete!<CR>", { desc = "Force delete buffer" })
+keymap("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Next buffer" })
+keymap("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
+keymap("n", "<leader>bl", "<cmd>ls<CR>", { desc = "List buffers" })
+keymap("n", "<leader>bf", "<cmd>bfirst<CR>", { desc = "First buffer" })
+keymap("n", "<leader>be", "<cmd>blast<CR>", { desc = "Last buffer" })
+
+-- Close all but current buffer
+keymap("n", "<leader>bo", function()
+  vim.cmd("%bd|e#|bd#")
+end, { desc = "Close all other buffers" })
+
+-- Conform
 vim.keymap.set("n", "<leader>cf", function() require("conform").format({ async = true, lsp_format = "fallback" }) end,
   { desc = "[F]ormat buffer" })
 
